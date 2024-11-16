6f50b7957c19eb48419e128ecd7837cec7468ba6
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2024-11-16 22:56:13 +0900

    Enabled reading config file for clangd

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -27,6 +27,7 @@ return {
           "clangd",
           "--background-index",
           "--clang-tidy",
+          "--enable-config",
           "--header-insertion=iwyu",
           "--completion-style=detailed",
           "--function-arg-placeholders",
