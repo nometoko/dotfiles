822f84049d89e824d47377fa04c8208d7314de86
Hidetaka Iwamoto <iwamoto@celegans.fun.bio.keio.ac.jp>
2024-11-18 17:49:56 +0900

    removed octo.nvim plugin for neovim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "plugins" },
   },
