d00be8be35900c7add86f88d0a5c580e296a5c46
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-04-13 11:46:59 +0900

    feat zsh: update zsh_options

--- zsh/.zsh/zsh_options.zsh
+++ zsh/.zsh/zsh_options.zsh
@@ -1,5 +1,7 @@
 setopt hist_reduce_blanks     # ignore blank
 setopt hist_ignore_all_dups   # don't save the same history to .zsh_history
+setopt share_history
+setopt autocd
 setopt numeric_glob_sort      # sort by number when numbers are included in file-names
 setopt CHASE_LINKS            # when you move to symbolic link, you will be move to the place it links
 setopt ignoreeof
