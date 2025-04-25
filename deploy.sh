21a64e590234c8eca578ab8c16641ae519d5d3cf
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-04-25 22:13:37 +0900

    fix dotfiles: refine deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,91 @@
-for files in $(ls -A | grep -vE '\.git$|\.gitignore$|deploy.sh$|zsh$|README.md$|.DS_Store$');
+#!/bin/sh
+
+for file in $(ls -A | grep -vE '\.git$|\.gitignore$|deploy.sh$|zsh$|README.md$|\.DS\_Store$|\.config$');
 do
-    if (test -f $HOME/$files); then
-        echo "File $files already exists in $HOME. Do you want to overwrite it? (y/n)"
-        read answer
-        if [ "$answer" != "${answer#[Yy]}" ] ;then
-            mv $HOME/$files $HOME/$files.bak
-            ln -sf $PWD/$files $HOME/$files
+    target=${HOME}/${file}
+    if [ -f $target ] || [ -d $target ]; then
+        if [ -L $target ]; then
+            resolved=$(readlink -f $target)
+            if [ "$resolved" != "$(readlink -f $file)" ]; then
+                echo -n "$target already exists. Do you want to overwrite it? [y/N]: "
+                read answer
+                if [ "$answer" != "${answer#[Yy]}" ] ;then
+                    unlink $file
+                    ln -sf $(readlink -f $file) $target
+                    echo "[symbolic link] $target -> $(readlink -f $file)"
+                fi
+            fi
+        else
+            echo "$target already exists. Do you want to overwrite it? (y/N)"
+            read answer
+            if [ "$answer" != "${answer#[Yy]}" ] ;then
+                mv $target ${target}.bak
+                ln -sf $(readlink -f $file) $target
+                echo "[symbolic link] $target -> $(readlink -f $file)"
+            fi
         fi
     else
-        ln -s $PWD/$files $HOME/$files
+        ln -s $(readlink -f $file) $target
+        echo "[symbolic link] $target -> $(readlink -f $file)"
     fi
 done
 
-for files in $(ls -A zsh);
+for file in $(find zsh -depth 1 | grep -vE '\.DS\_Store$')
 do
-    if (test -f $HOME/$files); then
-        echo "File $files already exists in $HOME. Do you want to overwrite it? (y/n)"
-        read answer
-        if [ "$answer" != "${answer#[Yy]}" ] ;then
-            mv $HOME/$files $HOME/$files.bak
-            ln -sf $PWD/zsh/$files $HOME/$files
+    target=${HOME}/$(basename $file)
+    if [ -f $target ] || [ -d $target ]; then
+        if [ -L $target ]; then
+            resolved=$(readlink -f $target)
+            if [ "$resolved" != "$(readlink -f $file)" ]; then
+                echo -n "$target already exists. Do you want to overwrite it? [y/N]: "
+                read answer
+                if [ "$answer" != "${answer#[Yy]}" ] ;then
+                    unlink $file
+                    ln -sf $(readlink -f $file) $target
+                    echo "[symbolic link] $target -> $(readlink -f $file)"
+                fi
+            fi
+        else
+            echo -n "$target already exists. Do you want to overwrite it? [y/N]: "
+            read answer
+            if [ "$answer" != "${answer#[Yy]}" ] ;then
+                mv $target ${target}.bak
+                ln -sf $(readlink -f $file) $target
+                echo "[symbolic link] $target -> $(readlink -f $file)"
+            fi
         fi
     else
-        ln -s $PWD/zsh/$files $HOME/$files
+        ln -s $(readlink -f $file) $target
+        echo "[symbolic link] $target -> $(readlink -f $file)"
+    fi
+done
+
+for file in $(find .config -depth 1 | grep -vE '\.DS\_Store$')
+do
+    target=${HOME}/${file}
+    if [ -f $target ] || [ -d $target ]; then
+        if [ -L $target ]; then
+            resolved=$(readlink -f $target)
+            if [ "$resolved" != "$(readlink -f $file)" ]; then
+                echo -n "$target already exists. Do you want to overwrite it? [y/N]: "
+                read answer
+                if [ "$answer" != "${answer#[Yy]}" ] ;then
+                    unlink $file
+                    ln -sf $(readlink -f $file) $target
+                    echo "[symbolic link] $target -> $(readlink -f $file)"
+                fi
+            fi
+        else
+            echo -n "$target already exists. Do you want to overwrite it? [y/N]: "
+            read answer
+            if [ "$answer" != "${answer#[Yy]}" ] ;then
+                mv $target ${target}.bak
+                ln -sf $(readlink -f $file) $target
+                echo "[symbolic link] $target -> $(readlink -f $file)"
+            fi
+        fi
+    else
+        ln -s $(readlink -f $file) $target
+        echo "[symbolic link] $target -> $(readlink -f $file)"
     fi
 done
