050098f12435ab9a9c549cc8799bb32d0aa4018c
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-04-25 22:29:29 +0900

    fix zsh: fixed .install.zsh

--- zsh/.install.zsh
+++ zsh/.install.zsh
@@ -1,9 +1,9 @@
-if not command -v starship &>/dev/null; then
+if command -v starship &>/dev/null; then
     echo "Starship is not installed. Installing..."
     curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir ~/bin --yes
 fi
 
-if not command -v sheldon &> /dev/null; then
+if command -v sheldon &> /dev/null; then
     echo "Sheldon is not installed. Installing..."
     # x86_64, aarch64, armv7, x86_64-apple-darwin
     curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh | bash -s -- --repo rossmacarthur/sheldon --to ~/bin
