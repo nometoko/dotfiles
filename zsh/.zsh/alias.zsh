03062b2fbb26f81793bc2ff3c121055a6a29d194
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-04-27 20:44:15 +0900

    fix zsh: refined alias.zsh

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -14,23 +14,9 @@ unset EmacsPath
 
 FijiPath='/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx'
 set_alias_if_exists $FijiPath fiji "$FijiPath"
-# alias fiji='/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx'
 unset FijiPath
 
-alias sz='source ~/.zshrc'
 set_alias_if_exists emacs ez 'emacs ~/.zshrc'
-# alias ez='emacs ~/.zshrc'
-
-set_alias_if_exists omz_history history ' omz_history'
-# alias history=" omz_history"       # don't save 'history' to .zsh_history
-
-jupyterPath="/opt/local/bin/jupyter-lab-3.11"
-set_alias_if_path_exists $jupyterPath jupyter "$jupyterPath"
-# alias jupyter="/opt/local/bin/jupyter-lab-3.11"
-unset jupyterPath
-
-alias c="clear"
-alias e="exit"
 
 if command -v lsd &>/dev/null; then
     alias ls='lsd'
@@ -38,61 +24,54 @@ else
     alias ls='/bin/ls -G -F --color=auto'
 fi
 
+alias c="clear"
+alias e="exit"
+alias l='ls -lAh'
 alias la='ls -A'
 alias lt='ls -t'
 alias lat='ls -At'
+alias ll='ls -lh'
 alias llt='ls -lt'
 alias llat='ls -lAt'
 alias -g G="| grep -i --color=auto"
 alias o.="open ."
 alias remake="make clean && make"
-
-if [ -z "`alias ll`" ]; then
-    alias ll='ls -lh'
-fi
-
-if [ -z "`alias l`" ]; then
-    alias l='ls -lAh'
-fi
+alias sz='source ~/.zshrc'
 
 if [ -f "$HOME/.config/kitty/kitty.conf" ]; then
     alias ek='emacs $HOME/.config/kitty/kitty.conf'
     alias ck='code $HOME/.config/kitty/kitty.conf'
 fi
 
+# direnv
 set_alias_if_exists direnv da 'direnv allow'
-# alias da='direnv allow'
+
+# tree
 set_alias_if_exists tree tree 'tree -a -I "\.DS_Store|\.git|\.venv"'
-# alias tree='tree -a -I "\.DS.Store|\.git|\.venv"'
+
+# vscode
 set_alias_if_exists code cz 'code ~/.zsh'
-# alias cz="code ~/.zshrc"
 set_alias_if_exists code c. 'code .'
 
+# nvim
 set_alias_if_exists nvim nz 'nvim ~/.zsh'
-
 set_alias_if_exists nvim n. 'nvim .'
 
 set_alias_if_exists icdiff diff 'icdiff -H -N'
 # alias diff="icdiff -H -N"
-set_alias_if_exists fzf fz 'fzf-noempty --bind "tab:toggle,shift-tab:toggle+beginning-of-line+kill-line,ctrl-j:toggle+beginning-of-line+kill-line,ctrl-t:top" --color=light -1 -m'
-# alias fz="fzf-noempty --bind 'tab:toggle,shift-tab:toggle+beginning-of-line+kill-line,ctrl-j:toggle+beginning-of-line+kill-line,ctrl-t:top' --color=light -1 -m"
+
+# fd
 set_alias_if_exists fd fd 'fd -E "CloudStorage"'
-# alias fd='fd -E "CloudStorage"'
-set_alias_if_exists fd clean-DS_Store 'fd -H .DS_Store -X rm -i'
-# alias clean-DS_Store='fd -H .DS_Store -X rm -i'
 
 # man likeなコマンド
 set_alias_if_exists tldr tldr 'tldr --pager'
-# alias tldr='tldr --pager'
 
 # 進化版cat
 set_alias_if_exists bat cat 'bat --plain'
 set_alias_if_exists bat less 'bat --plain'
-# alias cat='bat'
 
 # btm option
 set_alias_if_exists btm btm 'btm --theme default-light'
-# alias btm='btm --theme default-light'
 
 # lazygit
 set_alias_if_exists lazygit lz 'lazygit'
