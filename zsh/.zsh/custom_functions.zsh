2f3704767cbe2dfd8f1ba3e15921d7078d6c2a1c
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-04-27 20:45:28 +0900

    fix zsh: refined custom_functions.zsh

--- zsh/.zsh/custom_functions.zsh
+++ zsh/.zsh/custom_functions.zsh
@@ -5,14 +5,6 @@ done-notify() {
   osascript -e 'display notification "'"$var finished!"'" with title "Terminal"'
 }
 
-# for direnv
-show_virtual_env() {
-  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename $VIRTUAL_ENV))"
-  fi
-}
-export PS1='$(show_virtual_env)'$PS1
-
 # fd - cd to selected directory
 if [ -n "`command -v fzf 2>&1`" ]; then
   fcd() {
@@ -32,23 +24,6 @@ topdf(){
 
 del(){
   mv $1 ~/.Trash/
-}
-
-# pwd 直下のファイル名にスペースが含まれている場合、アンダーバーに置換
-replace-space(){
-  for file in "."/*; do
-      # ファイル名にスペースが含まれているか確認
-      if [[ "$file" == *" "* ]]; then
-          # 新しいファイル名を作成（スペースをアンダーバーに置換）
-          new_file=$(echo "$file" | tr ' ' '_')
-        
-          # ファイル名を変更
-          mv "$file" "$new_file"
-        
-          # 変更結果を表示
-          echo "Renamed: '$file' -> '$new_file'"
-      fi
-  done
 }
 
 venv () {
@@ -110,8 +85,4 @@ preexec() {
             echo "今田翔から逃れることはできない、、、"
         fi
     fi
-}
-
-d() {
-    dirs -v | head -10
 }
