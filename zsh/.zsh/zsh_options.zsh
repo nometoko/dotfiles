fc4c583946aa6aa59c5f64c7fb713ccce5b1335f
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-05-01 14:33:37 +0900

    feat zsh: added some options

--- zsh/.zsh/zsh_options.zsh
+++ zsh/.zsh/zsh_options.zsh
@@ -1,5 +1,6 @@
 setopt hist_reduce_blanks     # ignore blank
 setopt hist_ignore_all_dups   # don't save the same history to .zsh_history
+setopt hist_ignore_space
 setopt share_history
 setopt numeric_glob_sort      # sort by number when numbers are included in file-names
 setopt CHASE_LINKS            # when you move to symbolic link, you will be move to the place it links
@@ -10,4 +11,5 @@ setopt autoresume
 setopt noclobber
 setopt pushd_ignore_dups
 setopt globdots
+setopt interactive_comments
 unsetopt beep
