8585240521c56784572b41f53b76a1a0ec8d86cc
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-05-01 17:54:51 +0900

    feat zsh: ignore .DS_Store from completion

--- zsh/.zsh/zstyle.zsh
+++ zsh/.zsh/zstyle.zsh
@@ -1,5 +1,6 @@
 zstyle ':completion:*default' menu select=1
 zstyle ':completion:*' ignore-parents parent pwd ..    # ../ の後は今いるディレクトリを補間しない
 zstyle ':completion:*:cd:*' ignore-parents parent pwd  # 補間候補にカレントディレクトリは含めない
+zstyle ':completion:*:*:*:*:*' ignored-patterns '.DS_Store'
 zstyle ':completion:*' list-colors $LS_COLORS
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|=*' 'l:|=* r:|=*'
