cad18ea6480b0a1aec04e3a0e92b2ced73aa03a4
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-05-06 11:29:52 +0900

    feat zsh: modified completion style
    zstyleを設定するとfignoreが無視される

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -5,7 +5,7 @@ export WORDCHARS='*?_-.[]~=&;!#$%^(){}<'
 export EDITOR='vim'
 
 # 以下の拡張子を持つファイルは保管候補に出さない
-fignore=(.o .aux .log .bbl .blg .lof .dvi .fls .fdb_latexmk .synctex.gz .lot .toc .out .a\~)
+fignore=(.o .aux .log .bbl .blg .lof .dvi .fls .fdb_latexmk .synctex.gz .lot .toc .out .a\~ DS_Store)
 
 if [ -d /usr/libexec/java_home ]; then
     export JAVA_HOME=`/usr/libexec/java_home`
